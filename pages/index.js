c5e78ce4587d56aa613ab62eae9330fa5e6767eb
ABCXZY <rnrmfjc@gmail.com>
2022-12-11 21:52:28 +0900

    feat: 설명 추가

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import styled from "styled-components";
 import {useSession} from "next-auth/react";
 import userStore from "../store/user";
-
+import {Accordion} from "react-bootstrap";
+import leader from "../public/aaa.jpg"
+import Image from "next/image";
+import inputteam from "../public/input.png"
 const Index = () => {
   const {data: session} = useSession();
   const {userName, setUserName, userId, setUserId} = userStore()
@@ -11,33 +14,26 @@ const Index = () => {
 
   return (
     <div>
-      main page <br/>
-      사용방법!
+      <Container>
+        <h1>사용방법 안내</h1>
+        <Accordion defaultActiveKey={['0', '1']} alwaysOpen>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>팀장</Accordion.Header>
+            <Accordion.Body>
+              <h5>1. 팀 이름과 비밀번호를 입력해 생성해주세요.</h5>
+              <Image src={leader} alt={""}></Image>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1">
+            <Accordion.Header>팀원</Accordion.Header>
+            <Accordion.Body>
+             <h5>1. 팀 비밀번호를 입력 후 입장하기를 눌러주세요.</h5>
+              <Image src={inputteam}></Image>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
 
-      <H1 className={"text-3xl font-bold underline"}>팀장</H1>
-          <H2>1. 팀 생성해주세요.</H2>
-          <H2>2. 팀원에게 팀 코드를 보내주세요.</H2>
-      <hr/>
-      <H1>팀원</H1>
-        <H2>1. 팀장에게 받은 팀 코드를 입력해주세요.</H2>
-        <H2>2. 내 팀 목록에서 가입된 팀에 들어가주세요.</H2>
-      <hr/>
-      <H1>투두리스트 만들기</H1>
-
-      <hr/>
-      <H1>파일 관리하기</H1>
-
-      <hr/>
-      <H1>코멘트 달기</H1>
-
-      <hr/>
-      <H1>채팅하기</H1>
-      <h1></h1>
-      <H1></H1>
-      <hr/>
-      <H1>화상채팅하기</H1>
-
-      <hr/>
+      </Container>
     </div>
   )
 }
@@ -47,5 +43,9 @@ const H1 = styled.h1`
 
 const H2 = styled.h2`
     margin-left: 2vw;
+`
+
+const Container = styled.div`
+  padding: 3vw 20vw;
 `
 export default Index;
