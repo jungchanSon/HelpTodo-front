22a7248941ae51d42cfd3a3564b289f17c48ca82
jungchanSon <rnrmfjc@gmail.com>
2023-11-18 20:39:54 +0900

    feature: add team card component to insert team list

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -1,22 +1,67 @@
-const TeamCard = () => {
+import roomData from '../store/roomData'
+import Router from 'next/router'
+import todoTableStore from '../store/todoTableStore'
+import axios from 'axios'
+import { useState } from 'react'
+
+const TeamCard = ({ name, cDate, creator }) => {
+    const { teamName, setTeamName, setTeamCreator, setTeamCreateDate } = roomData()
+    const { todoTableData, setTodoTableData, is_reloadTodoTableData, off_Is_reloadTodoData } =
+        todoTableStore()
+
+    const enterTeamPage = (e) => {
+        e.preventDefault()
+        setTeamName(name)
+        setTeamCreateDate(cDate)
+        setTeamCreator(creator)
+
+        const reqParam = {
+            teamName: name,
+        }
+
+        axios
+            .post(process.env.NEXT_PUBLIC_ALL_TODOLIST, null, {
+                params: reqParam,
+            })
+            .then((res) => {
+                setTodoTableData(res.data)
+            })
+
+        Router.push('/todolist')
+    }
+
+    const exitTeamPage = (e) => {
+        e.preventDefault()
+
+        const reqParam = {
+            teamName: name,
+        }
+
+        axios.post(process.env.NEXT_PUBLIC_EXIT_TEAM, null, {
+            params: reqParam,
+        })
+        Router.reload()
+        Router.push('/')
+    }
 
     return (
-        <div
-            className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 lg:mx-auto'>
-            <div
-                className='mb-6 rounded-lg bg-white p-6 sm:mx-auto sm:w-full sm:max-w-sm mt-4 sm:mx-auto sm:w-full sm:max-w-sm md:max-w-md lg:max-w-lg'>
-                <h3>팀 이름</h3>
-                <div className='mt-2 flex'>
-                    <input
-                        id='password'
-                        name='password'
-                        type='password'
-                        autoComplete='current-password'
-                        required
-                        placeholder={'팀 비밀번호'}
-                        className='block w-5/6 mr-4 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                    />
-                    <button className='w-content'>참가</button>
+        <div className="min-h-full flex-1 flex-col justify-center px-6 lg:px-8 lg:mx-auto">
+            <div className="flex flex-row justify-content-between mb-6 rounded-lg bg-white p-6 sm:mx-auto sm:w-full sm:max-w-sm mt-4 sm:mx-auto sm:w-full sm:max-w-sm md:max-w-md lg:max-w-lg">
+                <h3>{name}</h3>
+                <div className="mt-2 flex border-so">
+                    <button
+                        className="w-content border-solid border-2 mx-2 border-red-600 rounded px-1 hover:bg-red-600 hover:text-white"
+                        onClick={exitTeamPage}
+                    >
+                        탈퇴
+                    </button>
+
+                    <button
+                        className="w-content border-solid border-2 border-indigo-300 rounded px-1 hover:bg-indigo-300 hover:text-white"
+                        onClick={enterTeamPage}
+                    >
+                        참가
+                    </button>
                 </div>
             </div>
         </div>
