460f10d4be8da2678c261ed89bdf4d1df9f8735e
jungchanSon <rnrmfjc@gmail.com>
2023-11-18 20:17:45 +0900

    feature: check token in root

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,24 @@ import '../styles/globals.css'
 import Header from '../components/header/Header'
 import { ThemeProvider } from 'styled-components'
 import defaultTheme from '../styles/Themes/theme'
-import { CookiesProvider } from 'react-cookie'
+import { CookiesProvider, useCookies } from 'react-cookie'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import GlobalBackground from '../styles/globalBackground'
+import axios from 'axios'
 
 function MyApp({ Component }) {
+    const [cookie] = useCookies(['token'])
 
+    // useEffect(() => {
+    //     if (cookie.token) {
+    //         axios.defaults.headers.common['Authorization'] = `Bearer ${cookie.token}`
+    //     }
+    // ), []}
+    if (cookie.token) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${cookie.token}`
+        console.log(axios.defaults.headers.common['Authorization'])
+    }
 
     return (
         <ThemeProvider theme={defaultTheme}>
